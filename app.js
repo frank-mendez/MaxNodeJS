0186f170d8ff8509301d1ae2a66019bcbf9b9919
frankmendez <frankmendezwebdev@gmail.com>
2020-11-26 18:40:34 +0800

    Sending Responses

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const http = require('http')
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers)
 	//process.exit()
+	res.setHeader('Content-Type', 'text/html')
+	res.write(
+		'<html><head><title>My First Page</title></head><body><h1>Hello from my Node.js Server</h1></body></html>'
+	)
+	res.end()
 })
 
 server.listen(5000)
