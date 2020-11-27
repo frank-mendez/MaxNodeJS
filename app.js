d8f0c844972ff9413261a43e5ea1b60212e92d9f
frankmendez <frankmendezwebdev@gmail.com>
2020-11-27 16:40:33 +0800

    Routing Requests

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers)
-	//process.exit()
-	res.setHeader('Content-Type', 'text/html')
-	res.write(
-		'<html><head><title>My First Page</title></head><body><h1>Hello from my Node.js Server</h1></body></html>'
-	)
+	const url = req.url
+
+	if (url === '/') {
+		res.setHeader('Content-Type', 'text/html')
+		res.write('<html>')
+		res.write('<head><title>My First Page</title></head>')
+		res.write(
+			'<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>'
+		)
+		res.write('</html>')
+		return res.end()
+	}
+
+	res.write('<html>')
+	res.write('<head><title>My First Page</title></head>')
+	res.write('<body><h1>Hello from my Node.js Server</h1></body>')
+	res.write('</html>')
 	res.end()
 })
 
