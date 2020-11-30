7b177a5849334ce8a8ac67a2bf6109d56339248f
frankmendez <frankmendezwebdev@gmail.com>
2020-11-30 19:10:00 +0800

    Block and Non blocking code

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ const server = http.createServer((req, res) => {
 			const parsedBody = Buffer.concat(body).toString()
 			console.log(parsedBody)
 			const message = parsedBody.split('=')[1]
-			fs.writeFileSync('message.txt', message)
-			res.writeHead(302, { Location: '/' })
-			return res.end()
+			fs.writeFile('message.txt', message, (err) => {
+				res.statusCode = 302
+				res.setHeader('Location', '/')
+				return res.end()
+			})
 		})
 	}
 	res.setHeader('Content-Type', 'text/html')
