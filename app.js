234f2121385aa4e0cd688d652776a175525f4919
frankmendez <frankmendezwebdev@gmail.com>
2020-12-04 19:37:26 +0800

    Installing Express.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http')
-const routes = require('./routes')
+const express = require('express')
 
-const server = http.createServer(routes)
+const app = express()
 
+const server = http.createServer(app)
 server.listen(5000)
